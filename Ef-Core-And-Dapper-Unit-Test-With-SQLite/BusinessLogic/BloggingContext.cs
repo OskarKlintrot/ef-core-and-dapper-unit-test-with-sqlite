c664bb8dca69b5b279c1cdca4685dfecbed34779
Oskar Klintrot <oskar.klintrot@p-bolaget.goteborg.se>
2019-06-14 14:33:24 +0200

    feat: Add code and tests from https://docs.microsoft.com/en-us/ef/core/miscellaneous/testing/

---
+++ Ef-Core-And-Dapper-Unit-Test-With-SQLite/BusinessLogic/BloggingContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
---
+++ Ef-Core-And-Dapper-Unit-Test-With-SQLite/TestProject/InMemory/BlogServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLogic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace TestProject.InMemory
+{
+    [TestClass]
+    public class BlogServiceTests
+    {
+        [TestMethod]
+        public void Add_writes_to_database()
+        {
+            var options = new DbContextOptionsBuilder<BloggingContext>()
+                .UseInMemoryDatabase(databaseName: "Add_writes_to_database")
+                .Options;
+
+            // Run the test against one instance of the context
+            using (var context = new BloggingContext(options))
+            {
+                var service = new BlogService(context);
+                service.Add("http://sample.com");
+            }
+
+            // Use a separate instance of the context to verify correct data was saved to database
+            using (var context = new BloggingContext(options))
+            {
+                Assert.AreEqual(1, context.Blogs.Count());
+                Assert.AreEqual("http://sample.com", context.Blogs.Single().Url);
+            }
+        }
+
+        [TestMethod]
+        public void Find_searches_url()
+        {
+            var options = new DbContextOptionsBuilder<BloggingContext>()
+                .UseInMemoryDatabase(databaseName: "Find_searches_url")
+                .Options;
+
+            // Insert seed data into the database using one instance of the context
+            using (var context = new BloggingContext(options))
+            {
+                context.Blogs.Add(new Blog { Url = "http://sample.com/cats" });
+                context.Blogs.Add(new Blog { Url = "http://sample.com/catfish" });
+                context.Blogs.Add(new Blog { Url = "http://sample.com/dogs" });
+                context.SaveChanges();
+            }
+
+            // Use a clean instance of the context to run the test
+            using (var context = new BloggingContext(options))
+            {
+                var service = new BlogService(context);
+                var result = service.Find("cat");
+                Assert.AreEqual(2, result.Count());
+            }
+        }
+    }
+}
